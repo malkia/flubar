ad3d0d7741f9cd3598af6401376eafd6d5e466d1
Dimiter 'malkia' Stanev <malkia@gmail.com>
2019-12-19 10:00:08 -0800

    fixed compilation errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart'
-    show debugDefaultTargetPlatformOverride;
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 
